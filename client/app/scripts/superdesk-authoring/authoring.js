c4f650245b6a64dcf5e2456ab4c66cdd528b26a1
akintolga <akint@aap.com.au>
2015-11-02 16:06:13 +1100

    [SD-3530] - Modify the list view to indicate master story is updated

--- client/app/scripts/superdesk-authoring/authoring.js
+++ client/app/scripts/superdesk-authoring/authoring.js
@@ -2308,8 +2308,8 @@
         };
     }
 
-    headerInfoDirective.$inject = ['familyService', 'authoringWidgets', 'authoring', 'archiveService'];
-    function headerInfoDirective(familyService, authoringWidgets, authoring, archiveService) {
+    headerInfoDirective.$inject = ['api', 'familyService', 'authoringWidgets', 'authoring', '$rootScope', 'archiveService'];
+    function headerInfoDirective(api, familyService, authoringWidgets, authoring, $rootScope, archiveService) {
         return {
             templateUrl: 'scripts/superdesk-authoring/views/header-info.html',
             require: '^sdAuthoringWidgets',
@@ -2335,6 +2335,12 @@
 
                         scope.activateWidget = function () {
                             WidgetsManagerCtrl.activate(relatedItemWidget[0]);
+                        };
+
+                        scope.previewMasterStory = function () {
+                            return api.find('archive', item.broadcast.master_id).then(function(item) {
+                                $rootScope.$broadcast('broadcast:preview', {'item': item});
+                            });
                         };
                     }
 
